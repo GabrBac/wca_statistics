6552c346e66ab9a0246c5927a517d809e2c2b5fa
GabrBac <116670830+GabrBac@users.noreply.github.com>
2025-10-25 00:02:34 +0200

    Fix country in rankings.rb

--- statistics/abstract/rankings.rb
+++ statistics/abstract/rankings.rb
@@ -19,7 +19,8 @@ class Rankings < GroupedStatistic
         average,
         value1, value2, value3, value4, value5,
         CONCAT('[', person.name, '](https://www.worldcubeassociation.org/persons/', person.wca_id, ')') person_link,
-        CONCAT('[', competition.cell_name, '](https://www.worldcubeassociation.org/competitions/', competition.id, ')') competition_link
+        CONCAT('[', competition.cell_name, '](https://www.worldcubeassociation.org/competitions/', competition.id, ')') competition_link,
+        country.name country,
       FROM results
       JOIN persons person ON person.wca_id = person_id AND person.sub_id = 1
       JOIN countries country ON country.id = person.country_id
